e5fecea2cfeabf17c52ffd4eaead81dbd7d893e0
Faris Jiwad <fjiwad@gwu.edu>
2025-01-31 10:34:50 -0500

    Got it to show in the sidebar YES FINALLY

--- src/extension.ts
+++ src/extension.ts
@@ -10,17 +10,30 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "deepseek-assistant" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('deepseek-assistant.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from deepseek_assistant!');
+	const myTreeView = vscode.window.createTreeView('deepseek_assistant.customView', {
+		treeDataProvider: new MyTreeDataProvider()
 	});
 
-	context.subscriptions.push(disposable);
+	const refreshCommand = vscode.commands.registerCommand('deepseek-assistant.refreshView', () => {
+		myTreeView.message = 'Refreshing...';
+	});
+
+	context.subscriptions.push(myTreeView, refreshCommand);
+}
+
+class MyTreeDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+	getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: vscode.TreeItem): Thenable<vscode.TreeItem[]> {
+		// Return an array of TreeItems to display in the view
+		return Promise.resolve([
+			new vscode.TreeItem('Item 1'),
+			new vscode.TreeItem('Item 2')
+		]);
+	}
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
